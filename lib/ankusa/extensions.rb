ff7b30d19534aaedd162dd2112dad77b057324f3
Brian Muller <brian.muller@livingsocial.com>
2010-12-03 11:28:06 -0500

    now catching any errors generated by rencoding utf-8 as ascii

--- lib/ankusa/extensions.rb
+++ lib/ankusa/extensions.rb
@@ -8,6 +8,6 @@ class String
   def to_ascii
     # from http://www.jroller.com/obie/tags/unicode
     converter = Iconv.new('ASCII//IGNORE//TRANSLIT', 'UTF-8') 
-    converter.iconv(self).unpack('U*').select { |cp| cp < 127 }.pack('U*')
+    converter.iconv(self).unpack('U*').select { |cp| cp < 127 }.pack('U*') rescue ""
   end
 end
