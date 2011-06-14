80c43ab034da7d49eabdd04d8b63020304b510a7
Alberto Leal <albertonb@gmail.com>
2011-06-14 19:15:58 -0300

    Adding some tests

--- lib/ankusa/hasher.rb
+++ lib/ankusa/hasher.rb
@@ -12,9 +12,17 @@ module Ankusa
       add_text(text) unless text.nil?
     end
 
+    def self.atomize(text)
+      text.downcase.to_ascii.tr('-', ' ').gsub(/[^\w\s]/," ").split
+    end
+
+    # word should be only alphanum chars at this point
+    def self.valid_word?(word)
+      return true unless Ankusa::STOPWORDS.include? word || word.length < 3 || word.numeric?
+    end
+
     def add_text(text)
-      #nb - duck typing the method just need to respond to each
-      if text.respond_to? :each
+      if text.instance_of? Array
         text.each { |t| add_text t }
       else
         # replace dashes with spaces, then get rid of non-word/non-space characters, 
@@ -25,24 +33,13 @@ module Ankusa
       self
     end
 
+    protected
+
     def add_word(word)
       @word_count += 1
       key = word.stem.intern
       store key, fetch(key, 0)+1
     end
-
-    def self.atomize(text)
-      text.downcase.to_ascii.tr('-', ' ').gsub(/[^\w\s]/," ").split
-    end
-
-    # word should be only alphanum chars at this point
-    def self.valid_word?(word)
-      return false if Ankusa::STOPWORDS.include? word
-      return false if word.length < 3
-      return false if word.numeric?
-      true
-    end
-
   end
 
 end
--- test/hasher_test.rb
+++ test/hasher_test.rb
@@ -1,9 +1,32 @@
 require File.join File.dirname(__FILE__), 'helper'
 
 class HasherTest < Test::Unit::TestCase
+  def setup
+    @text_hash = Ankusa::TextHash.new "Words word a the at fish fishing fishes? /^/  The at a of! @#$!"
+    @array = [@text_hash]
+  end
+
   def test_stemming
-    t = Ankusa::TextHash.new "Words word a the at fish fishing fishes? /^/  The at a of! @#$!"
-    assert_equal t.length, 2
-    assert_equal t.word_count, 5
+    assert_equal @text_hash.length, 2
+    assert_equal @text_hash.word_count, 5
+
+    if @array.respond_to? :each
+      @array.each do |a|
+        assert_equal a.length, 2
+        assert_equal a.word_count, 5
+      end
+    end
+  end
+
+  def test_valid_word
+    assert_nil Ankusa::TextHash.valid_word? "accordingly"
+    assert_nil Ankusa::TextHash.valid_word? "appropriate"
+    assert Ankusa::TextHash.valid_word? "^*&@"
+    assert Ankusa::TextHash.valid_word? "mother"
+    assert Ankusa::TextHash.valid_word? "21675"
+  end
+
+  def teardown
+    @text_hash, @array = nil
   end
 end
