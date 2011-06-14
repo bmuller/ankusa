ba4e354737152854b593e338752c2bd92c901cff
Brian Muller <brian.muller@livingsocial.com>
2011-06-14 19:54:21 -0400

    few stylistic modifications - removed unecessary setup/teardown in hash test

--- test/hasher_test.rb
+++ test/hasher_test.rb
@@ -3,19 +3,11 @@ require File.join File.dirname(__FILE__), 'helper'
 class HasherTest < Test::Unit::TestCase
   def setup
     @text_hash = Ankusa::TextHash.new "Words word a the at fish fishing fishes? /^/  The at a of! @#$!"
-    @array = [@text_hash]
   end
 
   def test_stemming
     assert_equal @text_hash.length, 2
     assert_equal @text_hash.word_count, 5
-
-    if @array.respond_to? :each
-      @array.each do |a|
-        assert_equal a.length, 2
-        assert_equal a.word_count, 5
-      end
-    end
   end
 
   def test_valid_word
@@ -24,9 +16,5 @@ class HasherTest < Test::Unit::TestCase
     assert Ankusa::TextHash.valid_word? "^*&@"
     assert Ankusa::TextHash.valid_word? "mother"
     assert Ankusa::TextHash.valid_word? "21675"
-  end
-
-  def teardown
-    @text_hash, @array = nil
   end
 end
