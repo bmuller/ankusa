eb8cfdc5320fd57db998b91bbcf130070ef1a69b
Alberto Leal <albertonb@gmail.com>
2011-06-15 07:07:46 -0300

    Fix wrong test

--- test/hasher_test.rb
+++ test/hasher_test.rb
@@ -2,20 +2,17 @@ require File.join File.dirname(__FILE__), 'helper'
 
 class HasherTest < Test::Unit::TestCase
   def setup
-    @text_hash = Ankusa::TextHash.new "Words word a the at fish fishing fishes? /^/  The at a of! @#$!"
-    @array = [@text_hash]
+    string = "Words word a the at fish fishing fishes? /^/  The at a of! @#$!"
+    @text_hash = Ankusa::TextHash.new string
+    @array = Ankusa::TextHash.new [string]
   end
 
   def test_stemming
     assert_equal @text_hash.length, 2
     assert_equal @text_hash.word_count, 5
 
-    if @array.respond_to? :each
-      @array.each do |a|
-        assert_equal a.length, 2
-        assert_equal a.word_count, 5
-      end
-    end
+    assert_equal @array.length, 2
+    assert_equal @array.word_count, 5
   end
 
   def test_valid_word
